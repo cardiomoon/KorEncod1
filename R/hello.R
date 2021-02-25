572af386344ebe130d591b8b22e420da5d2decc9
Keon-Woong Moon <cardiomoon@gmail.com>
2021-02-25 14:55:42 +0900

    0.0.1
    0.0.1

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' Sample Prob
+#'
+#' example of function containing Korean
+#' @importFrom rvest html_text
+#' @importFrom xml2 read_html
+#' @param url sample URL from DART system
+#' @export
+sampleprob <- function(url="http://dart.fss.or.kr/dsaf001/main.do?rcpNo=20200330003851") {
+
+  result <- grepl("연결재무제표 주석", html_text(read_html(url)))
+  return(result)
+}
